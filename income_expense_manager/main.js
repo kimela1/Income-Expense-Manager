a53fb7afbfac4344ea583102795087150954db59
Andy Situ <and.situ@gmail.com>
2019-10-23 23:03:49 -0700

    fixed spacing of main.js

--- income_expense_manager/main.js
+++ income_expense_manager/main.js
@@ -19,28 +19,28 @@ app.set('port', 58878);
 
 // Main Page: Running index
 app.get('/', function(res, res, next) {
-  var context = {title: "Income and Expense Manager"};
-  res.render('dashboard', context);
+    var context = {title: "Income and Expense Manager"};
+    res.render('dashboard', context);
 });
 
 app.get('/dashboard', function(res, res, next) {
-  var context = {title: "Dashboard"};
-  res.render('dashboard', context);
+    var context = {title: "Dashboard"};
+    res.render('dashboard', context);
 });
 
 app.get('/reports', function(res, res, next) {
-  var context = {title: "Reports"};
-  res.render('reports', context);
+    var context = {title: "Reports"};
+    res.render('reports', context);
 });
 
 app.get('/signUp', function(res, res, next) {
-  var context = {title: "sign up"};
-  res.render('signUp', context);
+    var context = {title: "sign up"};
+    res.render('signUp', context);
 });
 
 app.get('/transactions', function(res, res, next) {
-  var context = {title: "transactions"};
-  res.render('transactions', context);
+    var context = {title: "transactions"};
+    res.render('transactions', context);
 });
 
 // app.use(function(req, res){
@@ -54,5 +54,5 @@ app.get('/transactions', function(res, res, next) {
 // })
 
 app.listen(app.get('port'), function() {
-  console.log('Express started on http://localhost: ' + app.get('port'));
+    console.log('Express started on http://localhost: ' + app.get('port'));
 });
