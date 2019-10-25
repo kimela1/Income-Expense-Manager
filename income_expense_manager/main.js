e0e4319dfda814129e065e43edad0483e53e95c1
Andy Situ <and.situ@gmail.com>
2019-10-24 22:05:47 -0700

    Passport-Need to set up MySQL for User DB

--- income_expense_manager/main.js
+++ income_expense_manager/main.js
@@ -1,8 +1,49 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+
+// Authenticate to MySQL Server
+var mysql = require('./db.js');
+
+// Local Connection to MySQL
+// var mysql = require('./localdb.js');
+
 exphbs.create({defaultLayout:'main'});
 
 var app = express();
+
+var passport = require('passport'),
+    // Strategy (?) to authenticate requires
+    LocalStrategy = require('passport-local').Strategy;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        username.findOne( { username: username }, function(err, user) {
+            if (err) {return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.'});
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.'});
+            }
+
+            // Verify callback-Authenticates User
+            return done(null, user);
+        });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
 app.engine('handlebars', exphbs());
 
@@ -18,27 +59,27 @@ app.set('view engine', 'handlebars');
 app.set('port', 58878);
 
 // Main Page: Running index
-app.get('/', function(res, res, next) {
+app.get('/', function(req, res, next) {
     var context = {title: "Income and Expense Manager"};
     res.render('dashboard', context);
 });
 
-app.get('/dashboard', function(res, res, next) {
+app.get('/dashboard', function(req, res, next) {
     var context = {title: "Dashboard"};
     res.render('dashboard', context);
 });
 
-app.get('/reports', function(res, res, next) {
+app.get('/reports', function(req, res, next) {
     var context = {title: "Reports"};
     res.render('reports', context);
 });
 
-app.get('/signUp', function(res, res, next) {
+app.get('/signUp', function(req, res, next) {
     var context = {title: "sign up"};
     res.render('signUp', context);
 });
 
-app.get('/transactions', function(res, res, next) {
+app.get('/transactions', function(req, res, next) {
     var context = {title: "transactions"};
     res.render('transactions', context);
 });
@@ -51,7 +92,19 @@ app.get('/transactions', function(res, res, next) {
 // app.use(function(err, req, res, next) {
 //   res.status(400);
 //   res.render('500');
-// })
+// });
+
+app.get('/login', function(req, res, next) {
+    res.render('login/login');
+});
+
+app.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    })
+);
 
 app.listen(app.get('port'), function() {
     console.log('Express started on http://localhost: ' + app.get('port'));
