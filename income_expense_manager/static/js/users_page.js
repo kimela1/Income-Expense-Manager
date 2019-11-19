1b8cf8d2500e0b22fd77f25c1a58c3dd8185238d
Andy Situ <and.situ@gmail.com>
2019-11-18 18:33:46 -0800

    Dummy User's Page

---
+++ income_expense_manager/static/js/users_page.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function(e) {
+      var form = document.getElementById("users-form");
+      form.addEventListener("submit", function(e){ 
+          e.preventDefault();
+      })
+});
